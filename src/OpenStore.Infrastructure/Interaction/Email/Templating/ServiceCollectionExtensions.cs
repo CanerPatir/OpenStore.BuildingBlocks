dda4c8caabce7db06c21d9d6dc04c90471f4918b
Caner Patır <caner.patir@trendyol.com>
2020-11-23 19:09:51 +0300

    UseTemplate

--- src/OpenStore.Infrastructure/Interaction/Email/Templating/ServiceCollectionExtensions.cs
+++ src/OpenStore.Infrastructure/Interaction/Email/Templating/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace OpenStore.Infrastructure.Interaction.Email.Templating
@@ -11,10 +12,10 @@ namespace OpenStore.Infrastructure.Interaction.Email.Templating
             return services;
         }
         
-        public static MailBuilder UseTemplate<TModel>(this MailBuilder mailBuilder, IServiceProvider serviceProvider, string templateViewPath, TModel model)
+        public static async Task<MailBuilder> UseTemplate<TModel>(this MailBuilder mailBuilder, IServiceProvider serviceProvider, string templateViewPath, TModel model)
         {
             var razorViewToStringRenderer = serviceProvider.GetRequiredService<IRazorViewToStringRenderer>();
-            mailBuilder.UseHtmlBody().Body(razorViewToStringRenderer.RenderViewToStringAsync(templateViewPath, model).Result);
+            mailBuilder.UseHtmlBody().Body(await razorViewToStringRenderer.RenderViewToStringAsync(templateViewPath, model));
             return mailBuilder;
         }
     }
