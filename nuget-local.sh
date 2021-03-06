3c1e7d3d21391d20818fed04a50886fb7dcf93e9
Caner Patır <caner.patir@trendyol.com>
2021-03-06 21:12:20 +0300

    .

--- nuget-local.sh
+++ nuget-local.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-dotnet pack -c Release -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+dotnet pack -c Release -p:IncludeSymbols=false -p:SymbolPackageFormat=snupkg
 dotnet nuget push **/*.nupkg -s /Users/caner.patir/local_nuget --skip-duplicate
 dotnet nuget push **/*.snupkg -s /Users/caner.patir/local_nuget --skip-duplicate
---
+++ test/OpenStore.BB.TestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenStore.BB.TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
