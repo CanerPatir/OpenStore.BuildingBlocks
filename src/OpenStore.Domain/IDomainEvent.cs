172fe41005d507f68729b080bf64d149c85518be
Caner Patır <caner.patir@trendyol.com>
2021-09-24 00:49:25 +0300

    domain event abstraction

--- src/OpenStore.Domain/IDomainEvent.cs
+++ src/OpenStore.Domain/IDomainEvent.cs
@@ -1,6 +1,8 @@
+using MediatR;
+
 namespace OpenStore.Domain
 {
-    public interface IDomainEvent
+    public interface IDomainEvent : INotification
     {
         string Id { get; }
         long Version { get; set; }
