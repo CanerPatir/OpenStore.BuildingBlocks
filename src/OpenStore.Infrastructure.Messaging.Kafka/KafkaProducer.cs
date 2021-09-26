c41f2fff11e3d1f930cfbdde37d96798b3eecf41
Caner Patır <caner.patir@trendyol.com>
2021-09-26 13:30:31 +0300

    Outbox batch publish

--- src/OpenStore.Infrastructure.Messaging.Kafka/KafkaProducer.cs
+++ src/OpenStore.Infrastructure.Messaging.Kafka/KafkaProducer.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Confluent.Kafka;
@@ -20,7 +22,7 @@ namespace OpenStore.Infrastructure.Messaging.Kafka
             _logger = logger;
         }
 
-        public async Task Produce<TMessage>(string topic, string key, TMessage message, CancellationToken cancellationToken) 
+        public async Task Produce<TMessage>(string topic, string key, TMessage message, CancellationToken cancellationToken)
             where TMessage : class
         {
             var producer = _producerFactory.CreateWithKey<TMessage>();
@@ -43,7 +45,7 @@ namespace OpenStore.Infrastructure.Messaging.Kafka
             }
         }
 
-        public async Task Produce<TMessage>(string topic, TMessage message, CancellationToken cancellationToken) 
+        public async Task Produce<TMessage>(string topic, TMessage message, CancellationToken cancellationToken)
             where TMessage : class
         {
             var producer = _producerFactory.CreateWithNoKey<TMessage>();
@@ -65,73 +67,53 @@ namespace OpenStore.Infrastructure.Messaging.Kafka
             }
         }
 
-        public Task ProduceMany<TMessage>(string topic, string key, IEnumerable<TMessage> messages, CancellationToken cancellationToken) 
+        public async Task ProduceMany<TMessage>(string topic, string key, IEnumerable<TMessage> messages, CancellationToken cancellationToken)
             where TMessage : class
         {
             var producer = _producerFactory.CreateWithKey<TMessage>();
 
             using (_logger.BeginScope($"Kafka batch product w/ key {Guid.NewGuid()}"))
             {
-                void Handler(DeliveryReport<string, TMessage> r)
-                {
-                    if (r.Error.IsError)
-                    {
-                        _logger.LogInformation($"Bulk delivery item delivered with key {key} to '{r.TopicPartitionOffset}' {{}}", r);
-                    }
-                    else
-                    {
-                        _logger.LogError($"Batch delivery item failed: {r.Error.Reason}");
-                    }
-                }
-
                 foreach (var message in messages)
                 {
-                    producer.Produce(topic, new Message<string, TMessage>()
+                    var deliveryResult = await producer.ProduceAsync(topic, new Message<string, TMessage>()
                     {
                         Key = key,
                         Value = message,
                         Timestamp = new Timestamp(DateTime.UtcNow, TimestampType.LogAppendTime)
-                    }, Handler);
+                    }, cancellationToken);
+
+                    if (deliveryResult.Status == PersistenceStatus.NotPersisted)
+                    {
+                        throw new Exception($"Kafka message not persisted {message}");
+                    }
                 }
-
-                producer.Flush(cancellationToken);
             }
-
-            return Task.CompletedTask;
         }
 
-        public Task ProduceMany<TMessage>(string topic, IEnumerable<TMessage> messages, CancellationToken cancellationToken) 
+        public async Task ProduceMany<TMessage>(string topic, IEnumerable<TMessage> messages, CancellationToken cancellationToken)
             where TMessage : class
         {
             var producer = _producerFactory.CreateWithNoKey<TMessage>();
 
             using (_logger.BeginScope($"Kafka batch product wo/ key {Guid.NewGuid()}"))
             {
-                void Handler(DeliveryReport<Null, TMessage> r)
-                {
-                    if (r.Error.IsError)
-                    {
-                        _logger.LogInformation($"Bulk delivery item delivered without key to '{r.TopicPartitionOffset}' {{}}", r);
-                    }
-                    else
-                    {
-                        _logger.LogError($"Batch delivery item failed: {r.Error.Reason}");
-                    }
-                }
-
                 foreach (var message in messages)
                 {
-                    producer.Produce(topic, new Message<Null, TMessage>()
+                    var deliveryResult = await producer.ProduceAsync(topic, new Message<Null, TMessage>()
                     {
                         Value = message,
                         Timestamp = new Timestamp(DateTime.UtcNow, TimestampType.LogAppendTime)
-                    }, Handler);
+                    }, cancellationToken);
+
+                    if (deliveryResult.Status == PersistenceStatus.NotPersisted)
+                    {
+                        throw new Exception($"Kafka message not persisted {message}");
+                    }
                 }
 
                 producer.Flush(cancellationToken);
             }
-
-            return Task.CompletedTask;
         }
     }
 }
